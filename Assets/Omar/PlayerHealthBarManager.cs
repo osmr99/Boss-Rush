47f54e02fa022ab3b74a9902a58ab4746ae2546c
osmr99 <omarsantiagomorales@gmail.com>
2024-11-19 19:45:30 -0600

    Changed player hearts color
    (Without altering the original script)

---
+++ Assets/Omar/PlayerHealthBarManager.cs
@@ -0,0 +1,43 @@
+#pragma warning disable IDE0051
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.Rendering;
+
+namespace Omar
+{
+    
+    public class PlayerHealthBarManager : MonoBehaviour
+    {
+        [SerializeField] PlayerHealthDisplay healthDisplay;
+        List<Image> hearts;
+        [SerializeField] Color heartColor;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(UpdateHealthBar());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        IEnumerator UpdateHealthBar()
+        {
+            yield return new WaitForSeconds(3.0f);
+            healthDisplay.SetMaxHearts(15);
+            healthDisplay.UpdateHearts(0, 15);
+            foreach (Image heart in healthDisplay.GetComponentsInChildren<Image>())
+            {
+                heart.color = heartColor;
+            }
+        }
+    }
+}
