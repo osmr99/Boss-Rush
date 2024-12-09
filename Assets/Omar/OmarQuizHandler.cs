98bc8873bf84a5dadb954f5031a816f76b621cdf
osmr99 <omarsantiagomorales@gmail.com>
2024-12-09 02:37:40 -0600

    Functional Mechanic, now just apply it to the main game

--- Assets/Omar/OmarQuizHandler.cs
+++ Assets/Omar/OmarQuizHandler.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using DG.Tweening;
 using DG.Tweening.Plugins;
 using Unity.VisualScripting;
+using UnityEngine.InputSystem;
 
 namespace Omar
 {
@@ -18,12 +19,18 @@ namespace Omar
         [SerializeField] TMP_Text question;
         [SerializeField] Image timeBar;
         [SerializeField] GameObject timeBarParent;
+        [SerializeField] GameObject tLetter;
+        [SerializeField] GameObject fLetter;
+        [SerializeField] AudioClipCollection sounds;
         [SerializeField] public string[] questionList;
         [SerializeField] bool[] validAnswers;
         [SerializeField] float accuracyPercentage = 100;
         [SerializeField] float questionMaxTime;
         [SerializeField] bool timeBarIsActive;
+        [SerializeField] bool canAnswer = false;
         [SerializeField] float shakingMultiplier;
+        Vector3 scaleChange = new Vector3(1.25f, 1.25f, 1.25f);
+        bool gamepadConnected = false;
         int numOfAnsweredQuestions;
         float shakingPower;
         float randomX;
@@ -37,18 +44,40 @@ namespace Omar
         int randomNum;
 
 
+
         // Start is called before the first frame update
         void Start()
         {
-            wasThisQuestionAsked = new bool[questionList.Length];
             numOfAnsweredQuestions = 0;
             timeBarIsActive = false;
+            if (Gamepad.current != null)
+                gamepadConnected = true;
+            AskQuestion();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if(timeBarIsActive)
+            if(canAnswer)
+            {
+                Debug.Log("awaiting answer");
+                if(gamepadConnected)
+                {
+                    if (Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow) || Gamepad.current.rightShoulder.wasPressedThisFrame))
+                        CheckAnswer(true);
+                    else if(Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow) || Gamepad.current.leftShoulder.wasPressedThisFrame))
+                        CheckAnswer(false);
+                }
+                else
+                {
+                    if (Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow)))
+                        CheckAnswer(true);
+                    else if(Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow)))
+                        CheckAnswer(false);
+                }
+            }
+
+            if(timeBarIsActive && canAnswer)
             {
                 if(coloring == false)
                 {
@@ -62,25 +91,93 @@ namespace Omar
             }
         }
 
-        void AskQuestion()
+        void AnswerAnim(GameObject g)
         {
+            g.transform.localScale = scaleChange;
+            g.transform.DOScale(1, 0.2f).ForceInit();
+        }
+
+        public void AskQuestion()
+        {
+            ResetBar();
+            randomNum = Random.Range(0, questionList.Length);
             while (wasThisQuestionAsked[randomNum] == true)
             {
                 randomNum = Random.Range(0, questionList.Length);
             }
             question.text = questionList[randomNum];
+            SetMaxTime(randomNum);
             wasThisQuestionAsked[randomNum] = true;
+            canAnswer = true;
             if(numOfAnsweredQuestions > 2)
             {
                 StartCoroutine(TimeBarDelay());
-
             }
+        }
+
+        public void CheckAnswer(bool submitted)
+        {
+            numOfAnsweredQuestions++;
+            canAnswer = false;
+            if (submitted == true)
+                AnswerAnim(tLetter);
+            else
+                AnswerAnim(fLetter);
+            if(submitted == validAnswers[randomNum])
+                SoundEffectsManager.instance.PlayAudioClip(sounds.clips[0], true);
+            else
+                SoundEffectsManager.instance.PlayAudioClip(sounds.clips[1], true);
+            AskQuestion();
         }
 
         IEnumerator TimeBarDelay()
         {
             yield return new WaitForSeconds(1.5f);
             timeBarIsActive = true;
+        }
+
+        void TimeBarColor(float t)
+        {
+            timeBar.color = enoughTime;
+            timeBar.DOColor(noTime, t).ForceInit();
+        }
+
+        void TimeBarDrain()
+        {
+            if(elapsedTime > 0)
+            {
+                invertedTime += Time.deltaTime;
+                elapsedTime -= Time.deltaTime;
+                fillMath = (elapsedTime / questionMaxTime);
+                otherMath = (invertedTime / questionMaxTime);
+                timeBar.fillAmount = fillMath;
+                shakingPower = shakingMultiplier * otherMath;
+            }
+            else
+            {
+                AskQuestion();
+            }
+        }
+
+        void ShakingAnim()
+        {
+            randomX = Random.Range(0 + shakingPower, 0 - shakingPower);
+            randomY = Random.Range(0 + shakingPower, 0 - shakingPower);
+            timeBarParent.transform.localPosition = new Vector2(randomX, randomY);
+        }
+
+        void ResetBar()
+        {
+            if (DOTween.IsTweening(timeBar))
+                DOTween.Kill(timeBar);
+            StopAllCoroutines();
+            timeBar.color = enoughTime;
+            timeBarParent.transform.localPosition = new Vector2(0, 0);
+            timeBar.fillAmount = 1;
+            shakingPower = 0;
+            timeBar.color = enoughTime;
+            coloring = false;
+            timeBarIsActive = false;
         }
 
         void SetMaxTime(int index)
@@ -147,48 +244,7 @@ namespace Omar
                 case 19:
                     questionMaxTime = 15; //troll
                     break;
-
             }
-        }
-
-        void TimeBarColor(float t)
-        {
-            timeBar.color = enoughTime;
-            timeBar.DOColor(noTime, t);
-        }
-
-        void TimeBarDrain()
-        {
-            if(elapsedTime > 0)
-            {
-                invertedTime += Time.deltaTime;
-                elapsedTime -= Time.deltaTime;
-                fillMath = (elapsedTime / questionMaxTime);
-                otherMath = (invertedTime / questionMaxTime);
-                timeBar.fillAmount = fillMath;
-                shakingPower = shakingMultiplier * otherMath;
-            }
-            else
-            {
-                Reset();
-            }
-        }
-
-        void ShakingAnim()
-        {
-            randomX = Random.Range(0 + shakingPower, 0 - shakingPower);
-            randomY = Random.Range(0 + shakingPower, 0 - shakingPower);
-            timeBarParent.transform.localPosition = new Vector2(randomX, randomY);
-        }
-
-        void Reset()
-        {
-            timeBarParent.transform.localPosition = new Vector2(0, 0);
-            timeBar.fillAmount = 1;
-            shakingPower = 0;
-            timeBar.color = enoughTime;
-            coloring = false;
-            timeBarIsActive = false;
         }
     }
 }
