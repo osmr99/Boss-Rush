a12982fdd5f816c0b252c79d07090eb2a4108786
osmr99 <omarsantiagomorales@gmail.com>
2024-12-09 01:35:31 -0600

    Mechanic Important Commit

--- Assets/Omar/OmarQuizHandler.cs
+++ Assets/Omar/OmarQuizHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using DG.Tweening.Plugins;
+using Unity.VisualScripting;
 
 namespace Omar
 {
@@ -21,23 +22,27 @@ namespace Omar
         [SerializeField] bool[] validAnswers;
         [SerializeField] float accuracyPercentage = 100;
         [SerializeField] float questionMaxTime;
-        [SerializeField] bool timeBarIsActive = false;
+        [SerializeField] bool timeBarIsActive;
         [SerializeField] float shakingMultiplier;
+        int numOfAnsweredQuestions;
         float shakingPower;
         float randomX;
         float randomY;
-        bool[] wasThisQuestionsAsked;
+        [SerializeField] bool[] wasThisQuestionAsked;
         float elapsedTime;
         float invertedTime;
         float fillMath;
         float otherMath;
         bool coloring = false;
+        int randomNum;
 
 
         // Start is called before the first frame update
         void Start()
         {
-            wasThisQuestionsAsked = new bool[questionList.Length];
+            wasThisQuestionAsked = new bool[questionList.Length];
+            numOfAnsweredQuestions = 0;
+            timeBarIsActive = false;
         }
 
         // Update is called once per frame
@@ -54,6 +59,95 @@ namespace Omar
                 }
                 TimeBarDrain();
                 ShakingAnim();
+            }
+        }
+
+        void AskQuestion()
+        {
+            while (wasThisQuestionAsked[randomNum] == true)
+            {
+                randomNum = Random.Range(0, questionList.Length);
+            }
+            question.text = questionList[randomNum];
+            wasThisQuestionAsked[randomNum] = true;
+            if(numOfAnsweredQuestions > 2)
+            {
+                StartCoroutine(TimeBarDelay());
+
+            }
+        }
+
+        IEnumerator TimeBarDelay()
+        {
+            yield return new WaitForSeconds(1.5f);
+            timeBarIsActive = true;
+        }
+
+        void SetMaxTime(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    questionMaxTime = 5;
+                    break;
+                case 1:
+                    questionMaxTime = 14;
+                    break;
+                case 2:
+                    questionMaxTime = 5;
+                    break;
+                case 3:
+                    questionMaxTime = 7;
+                    break;
+                case 4:
+                    questionMaxTime = 9;
+                    break;
+                case 5:
+                    questionMaxTime = 5;
+                    break;
+                case 6:
+                    questionMaxTime = 10;
+                    break;
+                case 7:
+                    questionMaxTime = 6;
+                    break;
+                case 8:
+                    questionMaxTime = 2; //troll
+                    break;
+                case 9:
+                    questionMaxTime = 8;
+                    break;
+                case 10:
+                    questionMaxTime = 7;
+                    break;
+                case 11:
+                    questionMaxTime = 6;
+                    break;
+                case 12:
+                    questionMaxTime = 20; //troll
+                    break;
+                case 13:
+                    questionMaxTime = 20; //troll
+                    break;
+                case 14:
+                    questionMaxTime = 20; //troll
+                    break;
+                case 15:
+                    questionMaxTime = 8;
+                    break;
+                case 16:
+                    questionMaxTime = 10; //troll
+                    break;
+                case 17:
+                    questionMaxTime = 10;
+                    break;
+                case 18:
+                    questionMaxTime = 10;
+                    break;
+                case 19:
+                    questionMaxTime = 15; //troll
+                    break;
+
             }
         }
 
